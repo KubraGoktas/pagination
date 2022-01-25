f5ae5f09046409fb2ec3e888f680a950cd3d677c
KubraGoktas <kbrgkts1999@gmail.com>
2022-01-25 23:13:47 +0300

    'init'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Button } from 'reactstrap'
 import styles from './styles.module.css'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return( 
+  <div className={styles.test}>Example Component: {text}
+  <Button>Deneme</Button>
+  </div>
+  )
 }
